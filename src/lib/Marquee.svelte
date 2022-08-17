ee1b35df61b76ee9efb8458f67fc0d881bb5a191
RiverRay <laolei@forkway.cn>
2022-08-17 09:34:42 +0800

    chore: fixed type error

--- src/lib/Marquee.svelte
+++ src/lib/Marquee.svelte
@@ -1,25 +1,24 @@
 <!-- inspired by https://ryanmulligan.dev/blog/css-marquee/ -->
-
 <script lang="ts">
-  export let data: [];
-  export let gap = '60px';
-  export let speed = '5s';
+  export let data: any[];
+  export let gap = "60px";
+  export let speed = "5s";
 </script>
 
 <template>
-    <div class="marquee" style="--tail-gap: {gap};--tail-speed: {speed}">
-        <div class="marquee_content">
-            {#each data as item}
-                <div>{item}</div>
-            {/each}
-        </div>
-        <!-- Mirrors the content above -->
-        <div class="marquee_content" aria-hidden="true">
-            {#each data as item}
-                <div>{item}</div>
-            {/each}
-        </div>
+  <div class="marquee" style="--tail-gap: {gap};--tail-speed: {speed}">
+    <div class="marquee_content">
+      {#each data as item}
+        <div>{item}</div>
+      {/each}
     </div>
+    <!-- Mirrors the content above -->
+    <div class="marquee_content" aria-hidden="true">
+      {#each data as item}
+        <div>{item}</div>
+      {/each}
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
